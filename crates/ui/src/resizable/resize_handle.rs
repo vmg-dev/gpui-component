cd63b08182fce34addc57562ba43cf3447d6a777
Jason Lee <huacnlee@gmail.com>
2025-06-04 18:08:31 +0800

    Use deferred to render resize_handle drag area.

--- crates/ui/src/resizable/resize_handle.rs
+++ crates/ui/src/resizable/resize_handle.rs
@@ -1,15 +1,14 @@
 use std::{cell::RefCell, rc::Rc};
 
 use gpui::{
-    div, prelude::FluentBuilder as _, px, AnyElement, App, Axis, Element, ElementId, Entity,
-    GlobalElementId, InteractiveElement, IntoElement, MouseDownEvent, MouseUpEvent,
+    deferred, div, prelude::FluentBuilder as _, px, AnyElement, App, Axis, Element, ElementId,
+    Entity, GlobalElementId, InteractiveElement, IntoElement, MouseDownEvent, MouseUpEvent,
     ParentElement as _, Pixels, Point, Render, StatefulInteractiveElement, Styled as _, Window,
 };
 
-use crate::{dock::DockPlacement, ActiveTheme as _, AxisExt as _};
+use crate::{dock::DockPlacement, ActiveTheme as _, AxisExt as _, ContextModal};
 
-pub(crate) const HANDLE_PADDING: Pixels = px(4.);
-pub(crate) const HANDLE_SIZE: Pixels = px(1.);
+pub(crate) const HANDLE_SIZE: Pixels = px(5.);
 
 /// Create a resize handle for a resizable panel.
 pub(crate) fn resize_handle<T: 'static, E: 'static + Render>(
@@ -80,8 +79,13 @@ impl<T: 'static, E: 'static + Render> IntoElement for ResizeHandle<T, E> {
     }
 }
 
+pub struct RequestLayoutState {
+    handle: AnyElement,
+    resizable: bool,
+}
+
 impl<T: 'static, E: 'static + Render> Element for ResizeHandle<T, E> {
-    type RequestLayoutState = AnyElement;
+    type RequestLayoutState = RequestLayoutState;
     type PrepaintState = ();
 
     fn id(&self) -> Option<ElementId> {
@@ -99,13 +103,14 @@ impl<T: 'static, E: 'static + Render> Element for ResizeHandle<T, E> {
         window: &mut Window,
         cx: &mut App,
     ) -> (gpui::LayoutId, Self::RequestLayoutState) {
-        let neg_offset = -HANDLE_PADDING + HANDLE_SIZE / 2.;
+        let neg_offset = -(HANDLE_SIZE - px(1.)) / 2.;
+        let resizable = !(window.has_active_modal(cx) || window.has_active_drawer(cx));
         let axis = self.axis;
 
         window.with_element_state(id.unwrap(), |state, window| {
             let state = state.unwrap_or(ResizeHandleState::default());
 
-            let bg_color = if state.is_active() {
+            let border_color = if state.is_active() {
                 cx.theme().drag_border
             } else {
                 cx.theme().border
@@ -114,56 +119,98 @@ impl<T: 'static, E: 'static + Render> Element for ResizeHandle<T, E> {
             let mut el = div()
                 .id("handle")
                 .group("handle")
-                .occlude()
                 .absolute()
                 .flex_shrink_0()
-                .when_some(self.on_drag.clone(), |this, on_drag| {
-                    this.on_drag(
-                        self.drag_value.clone().unwrap(),
-                        move |_, position, window, cx| on_drag(&position, window, cx),
-                    )
+                .when(axis.is_horizontal(), |this| {
+                    this.top_0().left_0().h_full().w(HANDLE_SIZE)
+                })
+                .when(axis.is_vertical(), |this| {
+                    this.top_0().left_0().w_full().h(HANDLE_SIZE)
                 })
                 .map(|this| match self.placement {
-                    Some(DockPlacement::Left) => {
-                        // Special for Left Dock
-                        //  FIXME: Improve this to let the scroll bar have px(HANDLE_PADDING)
-                        this.cursor_col_resize()
-                            .top_0()
-                            .right(px(1.))
-                            .h_full()
-                            .w(HANDLE_SIZE)
-                            .pl(HANDLE_PADDING)
-                    }
-                    _ => this
-                        .when(axis.is_horizontal(), |this| {
-                            this.cursor_col_resize()
-                                .top_0()
-                                .left(neg_offset)
-                                .h_full()
-                                .w(HANDLE_SIZE)
-                                .px(HANDLE_PADDING)
-                        })
-                        .when(axis.is_vertical(), |this| {
-                            this.cursor_row_resize()
-                                .top(neg_offset)
-                                .left_0()
-                                .w_full()
-                                .h(HANDLE_SIZE)
-                                .py(HANDLE_PADDING)
-                        }),
+                    Some(DockPlacement::Left) => this.left_auto().right_0(),
+                    _ => this,
                 })
                 .child(
                     div()
-                        .bg(bg_color)
-                        .group_hover("handle", |this| this.bg(bg_color))
-                        .when(axis.is_horizontal(), |this| this.h_full().w(HANDLE_SIZE))
-                        .when(axis.is_vertical(), |this| this.w_full().h(HANDLE_SIZE)),
+                        .relative()
+                        .size_full()
+                        .map(|this| match self.placement {
+                            Some(DockPlacement::Left) => {
+                                this.border_color(border_color).border_r_1()
+                            }
+                            Some(DockPlacement::Right) => {
+                                this.border_color(border_color).border_l_1()
+                            }
+                            Some(DockPlacement::Bottom) => {
+                                this.border_color(border_color).border_t_1()
+                            }
+                            _ => this.child(
+                                div()
+                                    .absolute()
+                                    .bg(border_color)
+                                    .when(axis.is_horizontal(), |this| {
+                                        this.h_full().left_0().w(px(1.))
+                                    })
+                                    .when(axis.is_vertical(), |this| {
+                                        this.w_full().top_0().h(px(1.))
+                                    }),
+                            ),
+                        })
+                        .when(resizable, |this| {
+                            this.child(deferred(
+                                div()
+                                    .id("handle-dragger")
+                                    .group("handle")
+                                    .absolute()
+                                    .occlude()
+                                    .flex_shrink_0()
+                                    .when_some(self.on_drag.clone(), |this, on_drag| {
+                                        let Some(value) = self.drag_value.clone() else {
+                                            return this;
+                                        };
+
+                                        this.on_drag(value, move |_, position, window, cx| {
+                                            on_drag(&position, window, cx)
+                                        })
+                                    })
+                                    .when(axis.is_horizontal(), |this| {
+                                        this.cursor_col_resize()
+                                            .top_0()
+                                            .left(neg_offset)
+                                            .h_full()
+                                            .w(HANDLE_SIZE)
+                                    })
+                                    .when(axis.is_vertical(), |this| {
+                                        this.cursor_row_resize()
+                                            .left_0()
+                                            .top(neg_offset)
+                                            .w_full()
+                                            .h(HANDLE_SIZE)
+                                    })
+                                    .map(|this| match self.placement {
+                                        Some(DockPlacement::Left) => {
+                                            this.left_auto().right(neg_offset)
+                                        }
+                                        _ => this,
+                                    }),
+                            ))
+                        }),
                 )
                 .into_any_element();
 
             let layout_id = el.request_layout(window, cx);
 
-            ((layout_id, el), state)
+            (
+                (
+                    layout_id,
+                    RequestLayoutState {
+                        handle: el,
+                        resizable,
+                    },
+                ),
+                state,
+            )
         })
     }
 
@@ -176,7 +223,7 @@ impl<T: 'static, E: 'static + Render> Element for ResizeHandle<T, E> {
         window: &mut Window,
         cx: &mut App,
     ) -> Self::PrepaintState {
-        request_layout.prepaint(window, cx);
+        request_layout.handle.prepaint(window, cx);
     }
 
     fn paint(
@@ -189,7 +236,25 @@ impl<T: 'static, E: 'static + Render> Element for ResizeHandle<T, E> {
         window: &mut Window,
         cx: &mut App,
     ) {
-        request_layout.paint(window, cx);
+        request_layout.handle.paint(window, cx);
+
+        if !request_layout.resizable {
+            return;
+        }
+
+        let handle_size = HANDLE_SIZE - px(1.);
+        let pos_offset = match self.placement {
+            Some(DockPlacement::Left) => Point::new(handle_size / 2., px(0.)),
+            _ => {
+                if self.axis.is_horizontal() {
+                    Point::new(-handle_size / 2., px(0.))
+                } else {
+                    Point::new(px(0.), -handle_size / 2.)
+                }
+            }
+        };
+        let mut bounds = bounds;
+        bounds.origin = bounds.origin + pos_offset;
 
         window.with_element_state(id.unwrap(), |state: Option<ResizeHandleState>, window| {
             let state = state.unwrap_or(ResizeHandleState::default());
