fc9fde050e74e378d065dec9fd5f346e952bda27
Jason Lee <huacnlee@gmail.com>
2025-11-04 22:55:02 +0800

    chore: Remove unused image example. (#1510)
    Close #1494

--- crates/story/src/image_story.rs
+++ crates/story/src/image_story.rs
@@ -1,7 +1,7 @@
 use crate::section;
 use gpui::{
-    img, App, AppContext, Context, Entity, FocusHandle, Focusable, IntoElement, ParentElement as _,
-    Render, Styled, Window,
+    App, AppContext, Context, Entity, FocusHandle, Focusable, IntoElement, ParentElement as _,
+    Render, Styled, Window, img,
 };
 use gpui_component::{dock::PanelControl, v_flex};
 
@@ -47,20 +47,9 @@ impl Focusable for ImageStory {
 
 impl Render for ImageStory {
     fn render(&mut self, _: &mut Window, _: &mut Context<Self>) -> impl IntoElement {
-        // The svg file are from Assets
-        // See: crates/story/src/assets.rs#L21
-        v_flex()
-            .gap_4()
-            .size_full()
-            .child(section("SVG 160px").child(img("src/fixtures/google.svg").size_40().flex_grow()))
-            .child(
-                section("SVG 80px")
-                    .child(img("src/fixtures/color-wheel.svg").size_20().flex_grow()),
-            )
-            .child(
-                section("SVG from img 40px").child(
-                    img("https://pub.lbkrs.com/files/202503/vEnnmgUM6bo362ya/sdk.svg").h_24(),
-                ),
-            )
+        v_flex().gap_4().size_full().child(
+            section("SVG from URL")
+                .child(img("https://pub.lbkrs.com/files/202503/vEnnmgUM6bo362ya/sdk.svg").h_24()),
+        )
     }
 }
