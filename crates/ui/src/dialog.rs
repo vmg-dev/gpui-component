a71a3b977c68fc2bd556368a8bbfb9b4ca639e11
Jason Lee <huacnlee@gmail.com>
2025-12-15 11:27:07 +0800

    dialog: Fix top padding to match the other edges when not has title. (#1804)
    Close #1800
    
    | Before | After |
    | --- | --- |
    | <img width="524" height="155" alt="image"
    src="https://github.com/user-attachments/assets/2a1d677f-02e0-4331-b56e-6f2ceae3d528"
    /> | <img width="512" height="140" alt="image"
    src="https://github.com/user-attachments/assets/e41e70d6-9b0d-4810-84fe-a7de030bd838"
    /> |

--- crates/ui/src/dialog.rs
+++ crates/ui/src/dialog.rs
@@ -304,6 +304,7 @@ impl RenderOnce for Dialog {
         let on_close = self.on_close.clone();
         let on_ok = self.on_ok.clone();
         let on_cancel = self.on_cancel.clone();
+        let has_title = self.title.is_some();
 
         let render_ok: RenderButtonFn = Box::new({
             let on_ok = on_ok.clone();
@@ -393,6 +394,11 @@ impl RenderOnce for Dialog {
         }
         if let Some(pb) = self.style.padding.bottom {
             paddings.bottom = pb.to_pixels(base_size, rem_size);
+        }
+
+        if !has_title {
+            // When no title, reduce the top padding to fix line-height effect.
+            paddings.top -= px(6.);
         }
 
         let animation = Animation::new(Duration::from_secs_f64(0.25))
