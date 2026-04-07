e1f1edd2f364966d3dd75bbd40900f1932f49759
nihalar <51059485+nihalar@users.noreply.github.com>
2026-04-07 08:03:05 +0300

    highlighter: Fix Rust highlights regex (#2218)

--- crates/ui/src/highlighter/languages/rust/highlights.scm
+++ crates/ui/src/highlighter/languages/rust/highlights.scm
@@ -8,7 +8,7 @@
 
 ; Assume all-caps names are constants
 ((identifier) @constant
- (#match? @constant "^[A-Z][A-Z\\d_]+$'"))
+ (#match? @constant "^[A-Z][A-Z\\d_]+$"))
 
 ; Assume uppercase names are enum constructors
 ((identifier) @constructor
