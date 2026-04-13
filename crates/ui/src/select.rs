37ffc76e45f2a6becb33e512677a765e288a4b2c
George Waters <gwatersdev@gmail.com>
2026-04-13 05:09:02 -0400

    select: Focus Select when closing (#2237)
    ## Description
    
    When closing the select component, make it take focus back from the
    list. This helps when used in a dialog to not block other mouse
    interactions. This also helps with keyboard navigation.
    
    The defer is necessary to avoid the list recapturing focus if the select
    box is clicked on to close.
    
    ## Screenshot
    
    | Before                       | After                       |
    | ---------------------------- | --------------------------- |
    | <video
    src="https://github.com/user-attachments/assets/d55cda72-9ee7-423f-9634-a8d22a2302f1">
    | <video
    src="https://github.com/user-attachments/assets/99752b62-aba9-43d9-9967-a06bf11b29dc">
    |
    
    ## How to Test
    
    Use the gallery dialog example or the select example.
    
    ## Checklist
    
    - [x] I have read the [CONTRIBUTING](../CONTRIBUTING.md) document and
    followed the guidelines.
    - [x] Reviewed the changes in this PR and confirmed AI generated code
    (If any) is accurate.
    - [x] Passed `cargo run` for story tests related to the changes.
    - [ ] Tested macOS, Windows and Linux platforms performance (if the
    change is platform-specific)
    
    ---------
    
    Co-authored-by: Floyd Wang <gassnake999@gmail.com>

--- crates/ui/src/select.rs
+++ crates/ui/src/select.rs
@@ -1,9 +1,9 @@
 use gpui::{
     AnyElement, App, AppContext, Bounds, ClickEvent, Context, DismissEvent, Edges, ElementId,
     Entity, EventEmitter, FocusHandle, Focusable, InteractiveElement, IntoElement, KeyBinding,
-    Length, ParentElement, Pixels, Render, RenderOnce, SharedString,
-    StatefulInteractiveElement, StyleRefinement, Styled, Subscription, Task, WeakEntity, Window,
-    anchored, deferred, div, prelude::FluentBuilder, px, rems,
+    Length, ParentElement, Pixels, Render, RenderOnce, SharedString, StatefulInteractiveElement,
+    StyleRefinement, Styled, Subscription, Task, WeakEntity, Window, anchored, deferred, div,
+    prelude::FluentBuilder, px, rems,
 };
 use rust_i18n::t;
 
@@ -717,12 +717,15 @@ where
         cx.notify();
     }
 
-    fn escape(&mut self, _: &Cancel, _: &mut Window, cx: &mut Context<Self>) {
+    fn escape(&mut self, _: &Cancel, window: &mut Window, cx: &mut Context<Self>) {
         if !self.open {
             cx.propagate();
+            return;
         }
 
+        cx.stop_propagation();
         self.set_open(false, cx);
+        self.focus(window, cx);
         cx.notify();
     }
 
